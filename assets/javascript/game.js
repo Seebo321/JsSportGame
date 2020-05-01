1e4eaaae4c99ee468231a67d0edfa61b44f8986a
Samuel Campbell <seebo321@comcast.net>
2020-05-01 17:51:24 -0500

    updated all files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,13 @@ const resetNum = document.querySelector("#num-resets");
 const teamOneShots = document.querySelector("#teamone-numshots");
 const teamOneGoal = document.querySelector("#teamone-numgoals")
 const teamOneButton = document.querySelector("#teamone-shoot-button")
-
+// audio 
+const shootSound = document.getElementById("bounce");   
+const cheerSound = document.getElementById("cheer")
+const swishSound = document.getElementById("swish")
 // reset button
 resetButton.addEventListener("click", function () {
+    cheerSound.play(); 
     let newResetNum = Number(resetNum.innerHTML) + 1;
     resetNum.innerHTML = newResetNum;
 
@@ -24,7 +28,10 @@ resetButton.addEventListener("click", function () {
     
 })
 // team 1
-teamOneButton.addEventListener("click", function () {
+teamOneButton.addEventListener("click", function () { shootSound.play(); 
+  
+    
+
     let newTeamOneShots = Number(teamOneShots.innerHTML) + 1;
     teamOneShots.innerHTML = newTeamOneShots;;
   let x =  Math.floor(Math.random() * 101);
@@ -32,10 +39,12 @@ teamOneButton.addEventListener("click", function () {
     let newTeamOneGoal = Number(teamOneGoal.innerHTML) + 1;
     teamOneGoal.innerHTML = newTeamOneGoal;
     teamOneButton.className ="pass";
+    swishSound.play(); 
 }
 else{
     teamOneButton.className ="fail";
 }
+
 })
 // team 2
 const teamTwoShots = document.querySelector("#teamtwo-numshots");
@@ -43,6 +52,7 @@ const teamTwoGoal = document.querySelector("#teamtwo-numgoals")
 const teamTwoButton = document.querySelector("#teamtwo-shoot-button")
 
 teamTwoButton.addEventListener("click", function () {
+    shootSound.play();
     let newTeamTwoShots = Number(teamTwoShots.innerHTML) + 1;
     teamTwoShots.innerHTML = newTeamTwoShots;;
   let x =  Math.floor(Math.random() * 101);
@@ -50,6 +60,7 @@ teamTwoButton.addEventListener("click", function () {
     let newTeamTwoGoal = Number(teamTwoGoal.innerHTML) + 1;
     teamTwoGoal.innerHTML = newTeamTwoGoal;
     teamTwoButton.className ="pass";
+    swishSound.play(); 
 }
 else{
     teamTwoButton.className ="fail";
